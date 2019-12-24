d6bc5265c22c9e396fc37783c4244766a2a27c62
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2019-12-24 19:13:23 +0100

    Project Initial Commit - Simple Blockchain building

---
+++ BasicBlockhainAPI/BlockchainAPI/Block.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace BlockchainAPI
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+
+        public Block(DateTime timestamp, string previoushash, string data)
+        {
+            Index = 0;
+            TimeStamp = timestamp;
+            PreviousHash = previoushash;
+            Data = data;
+            Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] input = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}");
+            byte[] outputBytes = sha256.ComputeHash(input);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
---
+++ BasicBlockhainAPI/BlockchainAPI/Blockchain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace BlockchainAPI
+{
+    public class Blockchain
+    {
+        public IList<Block> Chain { get; set; }
+
+        public Blockchain()
+        {
+            InitializeChain();
+            CreateGenesisBlock();
+        }
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Hash = block.CalculateHash();
+            Chain.Add(block);
+        }
+    }
+}
